68449769a84f5fc1550fea02c5ab4523cdeab14a
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-05-23 19:04:07 +1000

    Added simple blocks

---
+++ Content.Shared/DrawDepth/DrawDepth.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared.Serialization;
+
+using DrawDepthTag = Robust.Shared.GameObjects.DrawDepth;
+
+namespace Content.Shared.DrawDepth;
+
+[ConstantsFor(typeof(DrawDepthTag))]
+public enum DrawDepth
+{
+    Blocks = DrawDepthTag.Default - 1,
+    
+    Objects = DrawDepthTag.Default,
+    
+    Items = DrawDepthTag.Default + 1,
+
+    Mobs = DrawDepthTag.Default + 2,
+}
---
+++ Content.Shared/Physics/CollisionGroup.cs
@@ -0,0 +1,21 @@
+﻿using Robust.Shared.Physics.Dynamics;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Physics;
+
+[Flags, FlagsFor(typeof(CollisionLayer)), FlagsFor(typeof(CollisionMask))]
+public enum CollisionGroup
+{
+    None = 0,
+    Opaque = 1 << 0,
+    Impassable = 1 << 1,
+    BulletImpassable = 1 << 2,
+    
+    // Masks
+    MobMask = Impassable,
+    BlockMask = Impassable,
+    
+    // Layers
+    MobLayer = Opaque | BulletImpassable,
+    BlockLayer = Opaque | Impassable | BulletImpassable,
+}
