84740b8bf8b99d7cfadffdaaa47f6d3c30861d97
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-05-25 12:32:59 +1000

    Updated sprites and add gravity

--- Content.Server/Game/GameLoop.Spawning.cs
+++ Content.Server/Game/GameLoop.Spawning.cs
@@ -1,6 +1,7 @@
 ﻿using System.Numerics;
 using Content.Shared.Game.Events;
 using Robust.Shared.Map;
+using Robust.Shared.Physics.Controllers;
 using Robust.Shared.Player;
 
 namespace Content.Server.Game;
@@ -8,19 +9,33 @@ namespace Content.Server.Game;
 public sealed partial class GameLoop
 {
     [Dependency] private readonly IMapManager _mapManager = default!;
+    [Dependency] private readonly Gravity2DController _gravity = default!;
+
+    private MapId TestMap
+    {
+        get
+        {
+            if (_map is not null)
+                return _map.Value;
+            
+            _map = _mapManager.CreateMap();
+            _gravity.SetGravity(_map.Value, new Vector2(0, -9.8f));
+
+            return _map.Value;
+        }
+    }
+
+    private MapId? _map;
     
     private void InitializeSpawning()
     {
-        
     }
     
     public void SpawnPlayer(ICommonSession session)
     {
         _sawmill.Debug($"Spawning {session.Name}");
-
-        var map = _mapManager.CreateMap();
         
-        var playerEntity = Spawn("Player", new MapCoordinates(Vector2.Zero, map));
+        var playerEntity = Spawn("Player", new MapCoordinates(Vector2.Zero, TestMap));
         _playerManager.SetAttachedEntity(session, playerEntity);
     }
 }
